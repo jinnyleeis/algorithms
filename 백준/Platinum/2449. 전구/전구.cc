1448c320dfc30cd09736969a65429dd9503685c8
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2025-02-18 18:38:50 +0900

    [Platinum III] Title: 전구, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Platinum/2449. 전구/전구.cc
+++ 백준/Platinum/2449. 전구/전구.cc
@@ -9,11 +9,9 @@ using namespace std;
 
 
 const int MAX=200;
-const int MAXCOLOR=20;
 
 int dp[MAX][MAX]; 
-// 특정 컬러로 바꾸는 것이 목표일 때, 변화 구간이 몇개 남았을 때, 바꾸는 최소 비용 
-// 구간이 짧으면 그거 dp로 가져와서 사용하고!! - 실제로 어디 위치인지는 중요하지 않다!!
+
 
 int N,K;
 
@@ -22,52 +20,42 @@ const int INF=1e9;
 int colors[MAX];
 
 
-// 일단 다 남았을 때, 최소 
-vector<int> spots;
 
 
-
+// 구간 곱 최소 비용 
 
 int main(){
     
-    
-    
-    
-    cin>>N >>K;
-    for(int i=0; i<N; i++){
-        cin>>colors[i];
-        if(i>=1&& colors[i]!=colors[i-1])spots.push_back(colors[i-1]);
-       
-    }
-     spots.push_back(colors[N-1]);
-    
-    int M=spots.size();
-    
-
-    
-   // memset(dp,INF,sizeof(dp)); 
-   // 제발.. memeset으로 inf 초기화 불가능하다ㅠㅠㅠ
-    
-  
-   for(int i = 0; i < MAX; i++){
-        for(int j = 0; j < MAX; j++){
-         dp[i][j] = (i == j ? 0 : INF);
-     
+    for(int i=0; i<MAX; i++){
+        for(int j=0; j<MAX; j++){
+            // 구간 1인건 여기서 처리됨
+            dp[i][j]= ((i==j) ? 0 : INF);
         }
     }
-
-  for(int l=2; l<=M; l++){
-      for(int i=0; i+l-1<M; i++){
-          int j =i+l-1;
-          for(int k=i; k<=(j-1); k++){
-              int cost=spots[i]==spots[j]? 0 : 1;
-            dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+cost);
-          }
-          
-          
-      }
-  }
     
-    cout<<dp[0][M-1]<<"\n";
+    cin>>N>>K;
+    
+    for(int i=0; i<N; i++){
+        cin>>colors[i];
+    }
+    
+    
+    for(int len=2; len<=N; len++){
+        int lastIndex=(N-1);
+        // 주의 3-3=0이다. 안헷갈리게  s<=lastIndex-len+1 이거 말고,
+        for(int s=0; s + (len - 1) < N; s++){
+            int e=s+len-1;
+            
+            //1개 1개 -> 총 2개 ~~~ N개(0부터~n-1까지)
+            for(int k=s; k<=(e-1); k++){
+                
+                int cost= (colors[s]==colors[k+1]) ? 0: 1;
+                dp[s][e]=min(dp[s][e], dp[s][k]+dp[k+1][e]+cost);
+            }
+            
+        }
+    }
+    
+    cout<<dp[0][N-1]<<"\n";
     return 0;
 }
