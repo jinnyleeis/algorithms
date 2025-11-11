55512851c0b72cddc87872c9462eb4b6612b8e5f
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2025-11-11 16:45:56 +0900

    [level 2] Title: 땅따먹기, Time: 569.95 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,47 +1,30 @@
-# 런타임 에러의 이유 : dp를 쓰는걸 깜빡함 정신차려라 친구야
-# 한 칸에서 다음 행으로 내려갈 때 3개 열로 분기 → 호출 수가 대략 4 * 3^(N-1) 수준으로 폭발하잖닝 그래서 dp 재귀꼴 만든건데 dp 오디가쏘
 
-"""
-문제의 처음부터 끝까지 항상 만족시키는 상태의 진행 단계
-row 0 1 2 ... N-2 N-1개의 state level이 존재함
-dp[row][col]의 설계 현재 레벨에서부터[(row,col)에서(row 레벨)] ~
-끝 레벨에까지(N-1,v(col)) 얻을 수 있는 최대 점수 
-
-"""
-
-import sys 
-sys.setrecursionlimit(200000)  # N 최대 100000 대비 여유
-
-def fn(c_row,c_col,N,land,dp):
-    if c_row == N: 
-        return 0
-    if dp[c_row][c_col]!=-1:
-        return dp[c_row][c_col]
-    
-    res=0
-    for n_col in range(0,4):
-        if n_col == c_col:
-            continue   
-        n_row = c_row+1
-        res = max(fn(n_row,n_col,N,land,dp)+land[c_row][c_col],res)
-        
-        dp[c_row][c_col]=res
-        
-    return dp[c_row][c_col]
-    
-    
 def solution(land):
     answer = 0
     
-    dp =[ [ -1 for c in row] for row in land]
+    dp = [ [-1 for col in row ] for row in land ]
     
     N=len(land)
-    #print(m)
     
+    # 주의 : 시작 레벨을 0이 아닌, 1로 해야. 
+    # 즉, 현재 레벨의(lv) 특정 변수를 variable로 (cur_col : frame의 옵션), lv-1까지는 고정된 블랙박스로.
+    # -> lv-1까지는 고정되었다고 보므로, 해당 고정값을 현재 레벨에서 불가능한 케이스를 걸러내는데 사용 (cur_col!=prev_col)
+    
+    # bottom-up에서 base case 설정하는거 까먹지 말아야 한다!!! - level 0은 이전 레벨이 없으므로 점화식 적용 불가.
     for col in range(0,4):
-        answer=max(answer,fn(0,col,N,land,dp))
+        dp[0][col] = land[0][col]
+    
+    for c_row in range(1,N): # 레벨기준. 현재까지의 최댓값
+        for c_col in range(0,4):
+            p_row=c_row-1
+            for p_col in range(0,4):
+                if c_col == p_col:
+                    continue
+                dp[c_row][c_col]=max(dp[p_row][p_col]+land[c_row][c_col],dp[c_row][c_col])
+    
+    for c_col in range(0,4):
+        answer=max(answer,dp[N-1][c_col])
         
-    print(answer)
-
+    
 
     return answer
